cea267c004dc42bf7f5f91175e30e1996ba69fd8
Ajaykv-10 <kvajai10@gmail.com>
2026-07-01 12:43:04 +0530

    filter in category and search

---
+++ react/day13/src/AppRoutes/AppRoutes.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AppRoutes = () => {
+  return (
+    <div>
+        <Routes>
+            <Routes path="/" element={<App />} />
+        </Routes>
+    </div>
+  )
+}
+
+export default AppRoutes
